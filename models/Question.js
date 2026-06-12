f76db76fbe4c39e910b21011672bc8d8301c7a5e
gabriell <gloxiniadrole890@gmail.com>
2026-06-12 16:12:20 +0700

    fix semua endpoint careers dan questions serta add 20 pertanyaan ke mongoDB

--- models/Question.js
+++ models/Question.js
@@ -8,11 +8,7 @@ const questionSchema = new mongoose.Schema({
     trait: {
         type: String,
         required: true
-    },
-    category: {
-        type: String,
-        default: 'Personality'
-    }
+    }   
 })
 
 module.exports = mongoose.model('Question', questionSchema);
---
+++ seedQuestions.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const Question = require("./models/Question");
+const questions = require("./data/questions");
+
+async function seedQuestions() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("MongoDB Connected!");
+
+    await Question.deleteMany();
+
+    console.log("Old questions deleted");
+
+    await Question.insertMany(questions);
+
+    console.log(`${questions.length} questions inserted`);
+
+    process.exit();
+  } catch (error) {
+    console.error(error);
+
+    process.exit(1);
+  }
+}
+
+seedQuestions();
